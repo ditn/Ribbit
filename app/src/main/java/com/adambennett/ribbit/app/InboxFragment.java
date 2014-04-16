1187b7d7021180a00bbdb552376e2c997314c3a7
Adam Bennett <incognito_6@msn.com>
2014-04-16 09:21:34 +0100

    Added SectionsPagerAdapter.class as well as FriendsFragment and InboxFragment and respective layouts

---
+++ app/src/main/java/com/adambennett/ribbit/app/InboxFragment.java
@@ -0,0 +1,23 @@
+package com.adambennett.ribbit.app;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by adambennett on 16/04/2014.
+ */
+public class InboxFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+
+
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/adambennett/ribbit/app/SectionsPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.adambennett.ribbit.app;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.support.v13.app.FragmentPagerAdapter;
+
+import java.util.Locale;
+
+/**
+ * Created by adambennett on 16/04/2014.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    protected Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        switch (position) {
+            case 0:
+                return new InboxFragment();
+            case 1:
+                return new FriendsFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.title_section1).toUpperCase(l);
+            case 1:
+                return mContext.getString(R.string.title_section2).toUpperCase(l);
+        }
+        return null;
+    }
+}
