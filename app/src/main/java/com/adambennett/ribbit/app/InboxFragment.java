993b07c911a621563ea693c2271e10d587989a1d
Adam Bennett <incognito_6@msn.com>
2014-05-07 13:45:38 +0100

    Added ImageView activity, drawables for message icons, MessageAdapter.class

--- app/src/main/java/com/adambennett/ribbit/app/InboxFragment.java
+++ app/src/main/java/com/adambennett/ribbit/app/InboxFragment.java
@@ -1,14 +1,17 @@
 package com.adambennett.ribbit.app;
 
 import android.app.ListFragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -52,7 +55,7 @@ public class InboxFragment extends ListFragment {
                         usernames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
                         i++;
                     }
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_list_item_1, usernames);
+                    MessageAdapter adapter = new MessageAdapter(getListView().getContext(), mMessages);
                     setListAdapter(adapter);
 
                 } else {
@@ -62,4 +65,23 @@ public class InboxFragment extends ListFragment {
         });
     }
 
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ParseObject message = mMessages.get(position);
+        String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+        ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
+        Uri fileUri = Uri.parse(file.getUrl());
+
+        if (messageType.equals(ParseConstants.TYPE_IMAGE)) {
+            // view the image
+            Intent intent = new Intent(getActivity(), ViewImageActivity.class);
+            intent.setData(fileUri);
+            startActivity(intent);
+        } else {
+            // view the video
+        }
+
+    }
 }
